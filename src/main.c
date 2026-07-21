9e62cba178d9fea11d0f2d69b934fb5142439c09
Hima <himapagolu@gmail.com>
2026-07-21 13:16:47 -0700

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -1,12 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
 
-  
+  //to keep REPL running
+  while(1){
+   
+  //standard $ sign 
   printf("$ ");
 
+  //input from user
+  char command[1024];
+  fgets(command,sizeof(command),stdin);
+
+  //to remove extra new line added when user presses enter
+  command[strcspn(command,"\n")] = '\0';
+
+  if(strncmp(command , "echo " , 5) == 0){
+    printf("%s/n",command + 5);
+  }
+  //to exit out of shell
+  else if(strcmp(command,"exit") == 0){
+    break;
+  }
+
+  printf("%s:command not found\n",command);
+
   return 0;
+}
 }
