a950b8a84c1421b0b40a24218f4191f2ea54d600
Hima <himapagolu@gmail.com>
2026-07-21 14:02:31 -0700

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -25,6 +25,14 @@ int main(int argc, char *argv[]) {
   //to exit out of shell
   else if(strcmp(command,"exit") == 0){
     break;
+  }
+  else if(strncmp(command,"type ",5) == 0){
+    if(strcmp(command + 5, "exit") == 0|| strcmp(command + 5, "echo") == 0 || strcmp(command + 5, "type") == 0){
+      printf("%s is a shell builtin\n" , command+5);
+    }else{
+      printf("%s : not found\n", command+5);
+    }
+    
   }else{
     printf("%s: command not found\n",command);
   }
